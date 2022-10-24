c1dbf7d99af0bef0d18575afbf23f76981918099
Alex Collado <alex.collado.gar@gmail.com>
2022-10-24 22:35:53 +0200

    Camara

--- Scripts/CharacterController.cs
+++ Scripts/CharacterController.cs
@@ -2,37 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Cinemachine;
 
 public class CharacterController : MonoBehaviour
 {
+    // "SerializeField" hace que un elemento aparezca en el Inspector de Unity aunque este en privado.
 
-    public Camera cam;
-    public NavMeshAgent player;
-    public Animator playerAnimator;
-    public GameObject targetDest;
+    [SerializeField] private Camera cam;
+    [SerializeField] private NavMeshAgent player;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private GameObject targetDest;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))    // Detectamos el click izquierdo del raton
         {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);    // Se dispara un rayo desde la camara a la posicion del raton
             RaycastHit hitPoint;
 
-            if(Physics.Raycast(ray, out hitPoint)){
-                targetDest.transform.position = hitPoint.point;
-                player.SetDestination(hitPoint.point);
+            if(Physics.Raycast(ray, out hitPoint))  // Si este rayo inpacta sobre cualquier geometria:
+            {                 
+                targetDest.transform.position = hitPoint.point;     // Se mueve el GameObject "targetDest" a la posicion en la que ha golpeado el rayo
+                player.SetDestination(hitPoint.point);              // El jugador se mueve hacia el esta posicion.
             }
         }
 
+        //  Maquina de estados para las animaciones
 
-        if(player.velocity != Vector3.zero){
+        if(player.velocity != Vector3.zero){    // Si la velocidad es no 0, se usa la animacion de caminar.
             playerAnimator.SetBool("isWalking", true);
             Debug.Log("Walking");
-        }else {
+        }else {                                 // En caso contrario, se usa la animacion de idle.
             playerAnimator.SetBool("isWalking", false);
             Debug.Log("Not Walking");
         }
-        
+    
     }
 }
