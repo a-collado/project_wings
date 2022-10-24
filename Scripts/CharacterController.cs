f39f428257c1522d29764d2536eb5c9f444567e6
Alex Collado <alex.collado.gar@gmail.com>
2022-10-24 18:28:42 +0200

    Primera versión del sistema de movimiento

---
+++ Scripts/CharacterController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent player;
+    public Animator playerAnimator;
+    public GameObject targetDest;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitPoint;
+
+            if(Physics.Raycast(ray, out hitPoint)){
+                targetDest.transform.position = hitPoint.point;
+                player.SetDestination(hitPoint.point);
+            }
+        }
+
+
+        if(player.velocity != Vector3.zero){
+            playerAnimator.SetBool("isWalking", true);
+            Debug.Log("Walking");
+        }else {
+            playerAnimator.SetBool("isWalking", false);
+            Debug.Log("Not Walking");
+        }
+        
+    }
+}
