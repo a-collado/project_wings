d69e534f299e1c1e2ed3fa9351f0f5ceeb7b3bd9
a-collado <alex.collado.gar@gmail.com>
2023-03-23 14:18:24 +0100

    Sistema de Button Prompts
    Faltaria hacer que el boton mire a la camara

---
+++ Scripts/Buttons/actionsEnum.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ActionsEnum
+{
+    NONE,
+    MOVE,
+    MOVE_FORWARD,
+    JUMP,
+    FALLOFF,
+    CROUCH,
+    SPRINT,
+    INTERACT,
+    POWER,
+    PAUSE
+}
