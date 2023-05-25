5763dc8b3e070144ead1dcdc733a534cdfd5d52b
Guillem Rosello Christiaen <guillem.rosello@students.salle.url.edu>
2023-05-25 10:56:22 +0200

    Audio Level 3 ext + Assets

---
+++ CustomAudioMixerEffect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomAudioMixerEffect : MonoBehaviour
+{
+    private AudioSource[] thunderSounds;
+    private AudioSource[] windSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlayAudio.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+
+    private AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void PlayAudioClip()
+    {
+        if (audio != null)
+            audio.PlayOneShot(audio.clip);
+    }
+}
